0a6b5129b2620029a1a4e93968894092aad327e5
Rito15 <ghdtkals15@naver.com>
2026-06-17 09:00:03 +0900

    타입스크립트 6버전에 맞게 구조 수정 중

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,3 @@
-import { TestUtil } from "./util/test.util";
-
-export function app() {
-    console.log("Success");
-    TestUtil();
-}
-
-// ESM
-if (import.meta.url?.endsWith(process.argv[1].replace(/\\/g, '/'))) {
-    app();
+export function app(){
+    console.log("start");
 }
---
+++ src/entries/README.md
@@ -0,0 +1,9 @@
+# Entries
+- 프로그램적 진입점이 되는 파일들
+- `main.ts` 또는 `main.?.ts` 꼴의 이름 형식
+- 별도 수정 필요 없음
+
+# App
+- app.ts
+- 기능적 진입점
+- `export function app(){...}`은 유지하되, 로직 수정 가능 
